c8e2ce1f2905c106a23a7cb67507b95bcb9c0acf
Nazarii Pirih <nazaryipyryg@gmail.com>
2022-03-31 20:15:20 +0300

    task2 - done

--- task2/index.js
+++ task2/index.js
@@ -1,4 +1,36 @@
-function calculate() {
-  //Here is your solution
+function grid(number) {
+  const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  let line;
+  let string;
+  let longAlphabetArr;
+
+  if (number <= 0) {
+    return null;
+
+  } else if (number <= alphabet.length) {
+
+    for (let i = 0; i < number; i++) {
+      line = alphabet.slice(i, number + i);
+      string = line.join('')
+      console.log(string);
+    }
+
+  } else if (number > alphabet.length) {
+    longAlphabetArr = alphabet.concat(alphabet, alphabet, alphabet);
+
+    for (let i = 0; i < number; i++) {
+      line = longAlphabetArr.slice(i, number + i);
+      string = line.join('')
+      console.log(string);
+    }
+  }
   return
 }
+
+grid(-2);
+
+grid(0);
+
+grid(25);
+
+grid(50);
