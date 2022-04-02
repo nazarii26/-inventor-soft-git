64f4be7484901e9b1d8787df9a187b989a500802
Nazarii Pirih <nazaryipyryg@gmail.com>
2022-04-02 11:40:22 +0300

    [ADD] fifth task done

--- js-ts/js/task5.js
+++ js-ts/js/task5.js
@@ -8,13 +8,13 @@ function partial(fn, ...partSave) {
 function add(a, b) { return a + b; }
 function mult(a, b, c, d) { return a * b * c * d; }
 
-var add5 = partial(add, 5); // Мы получили функцию с 1 аргументом, которая прибавляет к любому числу 5
+let add5 = partial(add, 5); // Мы получили функцию с 1 аргументом, которая прибавляет к любому числу 5
 
 console.log(add5(2)); // 7
 console.log(add5(10)); // 15
 console.log(add5(8)); // 13
 
-var mult23 = partial(mult, 2, 3); // мы зафиксировали первые 2 аргумента mult() как 2 и 3
+let mult23 = partial(mult, 2, 3); // мы зафиксировали первые 2 аргумента mult() как 2 и 3
 
 console.log(mult23(4, 5)); // 2*3*4*5 = 120
 console.log(mult23(1, 1)); // 2*3*1*1 = 6
