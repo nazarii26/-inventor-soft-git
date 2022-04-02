9906b4c40af392c18b1596001add7bef277ed0b0
Nazarii Pirih <nazaryipyryg@gmail.com>
2022-04-02 20:59:56 +0300

    [ADD] 5th task done

--- js-ts/ts/task5.ts
+++ js-ts/ts/task5.ts
@@ -2,22 +2,22 @@
 
 Intro:
 
-    Time to filter the data! In order to be flexible
-    we filter users using a number of criteria and
-    return only those matching all of the criteria.
-    We don't need Admins yet, we only filter Users.
+	 Time to filter the data! In order to be flexible
+	 we filter users using a number of criteria and
+	 return only those matching all of the criteria.
+	 We don't need Admins yet, we only filter Users.
 
 Exercise:
 
-    Without duplicating type structures, modify
-    filterUsers function definition so that we can
-    pass only those criteria which are needed,
-    and not the whole User information as it is
-    required now according to typing.
+	 Without duplicating type structures, modify
+	 filterUsers function definition so that we can
+	 pass only those criteria which are needed,
+	 and not the whole User information as it is
+	 required now according to typing.
 
 Higher difficulty bonus exercise:
 
-    Exclude "type" from filter criterias.
+	 Exclude "type" from filter criterias.
 
 */
 
@@ -40,34 +40,34 @@ export type Person = User | Admin;
 export const persons: Person[] = [
 	{ type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
 	{
-		 type: 'admin',
-		 name: 'Jane Doe',
-		 age: 32,
-		 role: 'Administrator'
+		type: 'admin',
+		name: 'Jane Doe',
+		age: 32,
+		role: 'Administrator'
 	},
 	{
-		 type: 'user',
-		 name: 'Kate Müller',
-		 age: 23,
-		 occupation: 'Astronaut'
+		type: 'user',
+		name: 'Kate Müller',
+		age: 23,
+		occupation: 'Astronaut'
 	},
 	{
-		 type: 'admin',
-		 name: 'Bruce Willis',
-		 age: 64,
-		 role: 'World saver'
+		type: 'admin',
+		name: 'Bruce Willis',
+		age: 64,
+		role: 'World saver'
 	},
 	{
-		 type: 'user',
-		 name: 'Wilson',
-		 age: 23,
-		 occupation: 'Ball'
+		type: 'user',
+		name: 'Wilson',
+		age: 23,
+		occupation: 'Ball'
 	},
 	{
-		 type: 'admin',
-		 name: 'Agent Smith',
-		 age: 23,
-		 role: 'Administrator'
+		type: 'admin',
+		name: 'Agent Smith',
+		age: 23,
+		role: 'Administrator'
 	}
 ];
 
@@ -77,20 +77,20 @@ export const isUser = (person: Person): person is User => person.type === 'user'
 export function logPerson(person: Person) {
 	let additionalInformation = '';
 	if (isAdmin(person)) {
-		 additionalInformation = person.role;
+		additionalInformation = person.role;
 	}
 	if (isUser(person)) {
-		 additionalInformation = person.occupation;
+		additionalInformation = person.occupation;
 	}
 	console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
 }
 
 export function filterUsers(persons: Person[], criteria: Partial<Omit<User, 'type'>>): User[] { // partial<type> вказує тип, який є необовʼязковий; omit<type,keys> видаляє ключі з type, назву ключа пишемо ''
 	return persons.filter(isUser).filter((user) => {
-		 const criteriaKeys = Object.keys(criteria) as (keyof Omit<User, 'type'>)[]; //omit<type,keys> видаляє ключі з type, назву ключа пишемо ''
-		 return criteriaKeys.every((fieldName) => {
-			  return user[fieldName] === criteria[fieldName];
-		 });
+		const criteriaKeys = Object.keys(criteria) as (keyof Omit<User, 'type'>)[]; //omit<type,keys> видаляє ключі з type, назву ключа пишемо ''
+		return criteriaKeys.every((fieldName) => {
+			return user[fieldName] === criteria[fieldName];
+		});
 	});
 }
 
@@ -99,10 +99,7 @@ console.log('Users of age 23:');
 filterUsers(
 	persons,
 	{
-		 age: 23
+		age: 23
 	}
 ).forEach(logPerson);
 
-// In case if you are stuck:
-// https://www.typescriptlang.org/docs/handbook/utility-types.html
-// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html#predefined-conditional-types
